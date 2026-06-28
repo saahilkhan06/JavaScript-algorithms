f4afeec574d198d66c187bbd8f6c41c1510ccee6
saahil khan <saahilslaptop@gmail.com>
2026-06-28 18:39:53 +0530

    finding facotrial of a number

---
+++ factorial.js
@@ -0,0 +1,10 @@
+// Problem: factorial of a number
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+console.log(factorial(5));
